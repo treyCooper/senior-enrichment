06e83550cb1181781353f86ee0c38722b2fc7717
Trey <treycoopermusic@gmail.com>
2017-12-06 12:15:07 -0500

    add associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,10 @@
 'use strict';
-
+const Student = require('./student');
+const Campus = require('./campus')
 const db = require('../index');
 
+Campus.hasMany(Student);
+Student.belongsTo(Campus);
 // Require all the models
 	// Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db
 	// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
