72d521d98ee0b4ea97717568befca446fd869544
Trey <treycoopermusic@gmail.com>
2017-12-06 12:07:10 -0500

    complete campus  model

--- server/db/models/campus.js
+++ server/db/models/campus.js
@@ -1,2 +1,16 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
+
+module.exports = db.define('campus', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:  'https://designerdoginfo.files.wordpress.com/2013/01/puggle-puppy-4.jpg?w=584'
+  },
+  description: {
+    type: Sequelize.TEXT
+  }
+}, {})
